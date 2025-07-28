eeb8c2da1175c03ee969846fffc5364fed71eef1
Pedro Benites Pirota Feitosa <pedrobpfeitosa@gmail.com>
2025-07-27 21:56:55 -0300

    Trocando banco de dados para o postgreSQL hospedado no render

--- DISRUPT_EJEM/Disrupt/settings.py
+++ DISRUPT_EJEM/Disrupt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,13 +78,16 @@ WSGI_APPLICATION = 'Disrupt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+   'default': dj_database_url.parse("postgresql://disrupt1_user:ewxuVXfYO63vHuofXPkMfYVEAsMKTXFL@dpg-d23c5jh5pdvs739qfdqg-a.virginia-postgres.render.com/disrupt1")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
