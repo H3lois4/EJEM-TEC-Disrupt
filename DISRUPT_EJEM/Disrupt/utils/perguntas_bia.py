7fc698149fd8394ee99d08335baa9c8d17788829
Marketing-principal <184695210+H3lois4@users.noreply.github.com>
2025-07-30 15:30:45 -0300

    Dicionário para a entrevista

---
+++ DISRUPT_EJEM/Disrupt/utils/perguntas_bia.py
@@ -0,0 +1,99 @@
+PERGUNTAS_BIA = {
+    'GERAL': {
+        'area': 'qual é a Área analisada?',
+        'macroprocesso': 'Qual é o Macroprocesso analisado?',
+        'processo': 'Qual é o Processo analisado?',
+        'subprocesso': 'Qual é o Subprocesso analisado?',
+        'descricao': 'Descreva resumidamente a atividade final!'   
+    },
+
+    'ASPECTOS_REGULATÓRIOS': {
+        'aspectos_regulatorios': 'Coloque aqui a lista de regulações às quais a plataforma deve estar submetida e gerar conformidade'
+    },
+
+    'ASPECTOS_CONTRATUAIS': {
+        'aspectos_regulatorios': 'Coloque aqui os aspectos contratuais que devem ser atendidos por constarem no contração de concessão da plataforma'
+    },
+
+    'INTERFACES_COM_OUTROS_PROCESSOS': {
+        'entradas': 'Digite o nome do processo/tarefa/produto e de onde veio, quando e o que (entradas):',
+        'saidas': 'Digite o nome do processo/tarefa/produto e para onde vai, quando e o que (saídas):',
+        'volumetrias': 'Digite as volumetrias relacionadas a entradas, execuções e saídas, e respectivas temporalidades'
+    },
+
+    'VOLUMETRIAS_CRÍTICAS_DO_PROCESSO': {
+        'volumetrias': 'Digite as volumetrias relacionadas a entradas, execuções e saídas, e respectivas temporalidades',
+        'SLAs': 'SLAs em horas'
+        },
+
+    'RECURSOS_HUMANOS': {
+        'rh': 'Descreva a equipe que executa o processo (quantidade de membros e cargos)'
+     },
+
+    'REGIME_DE_OPERAÇÃO_DA_ EQUIPE': {
+        'regime_operacao_equipe': 'Descreva os horários dos turnos e suas particularidades, se houver:'
+        }, 
+
+    'DEPENDÊNCIA_DE_RECURSOS_HUMANOS_CRÍTICOS': {
+        'equipe': 'Descreva a equipe que é indispenável para a execução do processo:',
+        'contingencias': 'Descreva as contingências atuais',
+        'SLAs': 'SLAs em horas'
+        },
+
+    'FORNECEDORES': {
+        'fornecedores': 'Dê o nome dos fornecedores e sua devida tarefa de RH, Insumos, equipamentos e TI'
+        },
+
+    'DEPENDÊNCIA_DE_INSUMOS_CRÍTICOS': {
+        'lead_time': 'Citar o Lead Time (dias até a chegada do produto):',
+        'contingencias': 'Descreva as contingências atuais',
+        'SLAs': 'SLAs em horas'
+        },
+
+    'SISTEMAS_TI_TA': {
+        'sistemas_ti_ta': 'Citar todos os sistemas críticos, na visão do usuário:'
+    },
+    
+    'DEPENDENCIA_SISTEMAS_TI_TA_CRITICOS': {
+        'sistemas_criticos_usuario': 'Citar os sistemas que são entendidos como críticos na visão do usuário',
+        'contingencias': 'Descreva as contingências atuais',
+        'SLAs': 'SLAs em horas'
+    },
+    
+    'EQUIPAMENTOS_UTILIZADOS': {
+        'equipamentos': 'Citar os equipamentos utilizados no processo'
+    },
+    
+    'EQUIPAMENTOS_CRITICOS': {
+        'equipamentos_criticos': 'Citar os equipamentos críticos utilizados no processo',
+        'SLAs': 'SLAs em horas'
+    },
+    
+    'CRITICIDADE_PROCESSO': {
+        'pontos_criticidade': 'Citar pontos do processo/ativo que possam gerar indisponibilidade'
+    },
+    
+    'PIOR_CENARIO': {
+        'pior_cenario': 'Descrever o pior cenário possível (considerando Período Crítico, Sazonalidade, etc (exceto fator humano):'
+    },
+    
+    'MTPD': {
+        'mtpd_horas': 'Informar o MTPD (Maximum Tolerable Period of Disruption) em horas'
+    },
+    
+    'MOTIVADORES_MTPD': {
+        'descricao_motivo_mtpd': 'Descrição do motivo para definição do MTPD'
+    },
+    
+    'RTO': {
+        'rto_horas': 'Informar o RTO (Recovery Time Objective) em horas'
+    },
+    
+    'RPO': {
+        'rpo_horas': 'Informar o RPO (Recovery Point Objective) em horas'
+    },
+    
+    'OBSERVACOES': {
+        'observacoes': 'Espaço para observações adicionais'
+    }
+}
